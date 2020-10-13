00ab296a992f13a4d33f38234a324493b11546b4
skrall3119 <alexjanci@yahoo.com>
2020-10-13 13:07:23 -0500

    Initial Commit

---
+++ SaveMyData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveMyData
+//
+//  Created by Alex Janci on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
